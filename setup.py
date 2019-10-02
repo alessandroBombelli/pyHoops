a291fab1e19ecf6820dcced9a1367b480fbf1f83
Alessandro Bombelli <alessandro.bombelli.87@gmail.com>
2019-10-02 14:11:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,7 @@ setup(
   url = 'https://github.com/alessandroBombelli/pyHoops',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['DATA ANALYTICS', 'WEB-PARSING', 'BASKETBALL'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'requests',
-          'beautifulsoup4',
-          'pandas',
-          'sys',
-          'os',
+  install_requires=[
           'numpy',
           'matplotlib'
       ],
