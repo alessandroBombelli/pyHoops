eb2cfe2c11547005213c0c1db0bb341011c86651
Alessandro Bombelli <alessandro.bombelli.87@gmail.com>
2019-10-02 14:01:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pyHoops',         # How you named your package folder
   packages = ['pyHoops'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'a python package for advanced basket data analytics',   # Give a short description about your library
   author = 'alessandroBombelli',                   # Type in your name
