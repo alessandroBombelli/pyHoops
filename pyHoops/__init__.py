9ca5e6bb719e588fe0ab8970bebfa0afcacbd766
Alessandro Bombelli <alessandro.bombelli.87@gmail.com>
2019-10-04 10:03:24 +0200

    Add files via upload
    Slightly modified package. Current release is v1.5

---
+++ pyHoops/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 17:36:35 2019
+
+@author: alessandro
+"""
+
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from matplotlib.cbook import get_sample_data
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import random
+import re
+import itertools
+import unicodedata
+from pyHoops import pyHoops
+
+            
+
